e1f92acdd13024ac64ee6fb56f5d4d62327b87fc
vineet dixit <vineetdixit.vns@gmail.com>
2024-09-25 10:42:29 -0700

    Added new question

--- index.js
+++ index.js
@@ -147,3 +147,50 @@ function longest(s1, s2) {
         return false 
         }
         }
+
+
+        // Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+        // Examples:
+        
+        // * 'abc' =>  ['ab', 'c_']
+        // * 'abcdef' => ['ab', 'cd', 'ef']
+
+        function solution(str){
+            let arr=[]
+            let num=0
+        const newStr=str.split("")
+        if(newStr.length%2!==0){
+            newStr.push("_")
+        }
+        for(let i=0;i<newStr.length/2;i++){
+            arr.push(newStr[num]+newStr[num+1])
+            num+=2
+        }
+        return arr
+        
+        }
+
+
+
+//         Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+// For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+// The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+// Based on: https://leetcode.com/problems/two-sum/
+
+// twoSum([1, 2, 3], 4) // returns [0, 2] or [2, 0]
+// twoSum([3, 2, 4], 6) // returns [1, 2] or [2, 1]
+
+
+        function twoSum(numbers, target) {
+            for(let i=0;i<numbers.length;i++){
+                 for(let j=1;j<numbers.length;j++){
+           if(numbers[i]+numbers[j]=== target){
+           return [i,j]
+           }
+           }
+           }
+           }
