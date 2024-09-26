a8b8deb1e9a39161411ea7494ac66c909a84e400
vineet dixit <vineetdixit.vns@gmail.com>
2024-09-26 10:31:49 -0700

    Wip:rot13

--- index.js
+++ index.js
@@ -194,3 +194,28 @@ function longest(s1, s2) {
            }
            }
            }
+
+        //    ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+
+        //    Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+
+           function rot13(message){
+            //your code here
+          const arr=message.split("")
+          // console.log(arr)
+          let newarr=[]
+          for(let i=0;i<arr.length;i++){
+          
+          // console.log(charCodeAt(arr[i]))
+          if(arr[i].charCodeAt(0)>64 && arr[i].charCodeAt(0)<91 || arr[i].charCodeAt(0)>96 && arr[i].charCodeAt(0)<123 ){
+          
+          const charCode=arr[i].charCodeAt(0) + 13
+          console.log( charCode)
+          console.log(String.fromCharCode(charCode))
+          newarr.push(String.fromCharCode(charCode))
+          }
+          
+          }
+          
+          console.log(newarr)
+          }
