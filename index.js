da49c71935ef1a93eeea768460b80f0d2d8ea6ca
vineet dixit <vineetdixit.vns@gmail.com>
2024-10-04 11:41:26 -0700

    changed

--- index.js
+++ index.js
@@ -226,3 +226,30 @@ function longest(s1, s2) {
             return newarr.join("");
         }
         
+
+//         Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+function duplicateCount(text){
+    const val=text.toLowerCase().split("")
+    let obj={}
+    for(let i=0;i<val.length;i++){
+      if(obj[val[i]]){
+          obj[val[i]]+=1
+      }else{
+          obj[val[i]]=1
+      }
+    }
+     return Object.values(obj).filter((item)=>{
+        return item>1
+     }).length
+    }
